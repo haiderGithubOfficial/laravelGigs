4216e7f8125a3ea96343bb2f6cdc234ff9d838fc
haiderGithubOfficial <haider.official.gfx@gmail.com>
2022-11-29 10:12:23 +0500

    first commit

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use PhpParser\Node\Expr\List_;
+
+class ListingController extends Controller
+{
+
+    // to show all listing
+    public function index()
+    {
+        return view('listings.index', [
+            'heading' => 'Latest Listing',
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(4),
+        ]);
+    }
+
+    //to show one single listing
+    public function show(Listing $listing)
+    {
+        return view('listings.show', [
+            'listing' => $listing,
+        ]);
+    }
+
+    //to show create list item form
+    public function create()
+    {
+        return view('listings.create');
+    }
+
+    //to store a list item
+    public function store(Request $request)
+    {
+        // dd($request->file('logo'));
+
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique(
+                'listings',
+                'company'
+            )],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $formFields['user_id'] = auth()->id();
+
+        Listing::create($formFields);
+
+
+        return (redirect('/')->with('message', 'Listing created successfully!'));
+    }
+
+    //show edit form
+    public function edit(Listing $listing)
+    {
+        return view('listings.edit', ['listing' => $listing]);
+    }
+
+
+    //to update a list item
+    public function update(Request $request, Listing $listing)
+    {
+        //Make sure logged in user is owner
+        if ($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => 'required',
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+
+
+        return (back()->with('message', 'Listing updated successfully!'));
+    }
+
+
+    public function destroy(Listing $listing)
+    {
+        //Make sure logged in user is owner
+        if ($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+
+        $listing->delete();
+        return redirect('/')->with('message', 'GIG deleted Successfully!');
+    }
+
+    // Manage Listings
+    public function manage()
+    {
+        return view('listings.manage', ['listings' => auth()->user()->listings()->get()]);
+    }
+}
